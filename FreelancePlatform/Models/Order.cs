c2dd5d2bce46ac0e1d736174a0950a907bd8c076
bogdan-iontcaciuc <bt5717219@gmail.com>
2026-05-04 21:41:37 +0300

    Add project files.

---
+++ FreelancePlatform/Models/Order.cs
@@ -0,0 +1,10 @@
+﻿namespace FreelancePlatform.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int ServiceId { get; set; }
+        public int ClientId { get; set; }
+        public string Status { get; set; }
+    }
+}
---
+++ FreelancePlatform/Models/Service.cs
@@ -0,0 +1,11 @@
+﻿namespace FreelancePlatform.Models
+{
+    public class Service
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int UserId { get; set; }
+    }
+}
